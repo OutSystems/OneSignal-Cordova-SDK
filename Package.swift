1e8346c61f872ccaece57f5e8cc0a91fc12dc01c
OS-pedrogustavobilro <pedro.gustavo.bilro@outsystems.com>
2026-04-21 11:25:03 +0100

    feat(ios): Add Package.swift file
    Still to review and test if the cordova ios dependency on Package.swift is correct or not.
    
    References:
    - https://outsystemsrd.atlassian.net/browse/RMET-5135
    - https://outsystemsrd.atlassian.net/wiki/spaces/RDMBLVS/pages/6218481742/SPM+CocoaPods+API+Clash+Risk+Evaluation
    - https://outsystemsrd.atlassian.net/wiki/spaces/RDMBLVS/pages/5398003879/Technical+Analysis+Swift+Package+Manager+SPM+Support+for+Cordova+iOS+8+Plugins
    - https://cordova.apache.org/docs/en/latest/guide/platforms/ios/plugin.html#supporting-swift-package-manager-spm

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "onesignal-cordova-plugin",
+    platforms: [.iOS(.v15)],
+    products: [
+        .library(
+            name: "onesignal-cordova-plugin",
+            targets: ["OneSignalPushPlugin"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apache/cordova-ios.git", branch: "master"),
+        .package(
+            url: "https://github.com/OutSystems/OneSignal-iOS-SDK.git",
+            exact: "2.16.7-test.2")
+    ],
+    targets: [
+        .target(
+            name: "OneSignalPushPlugin",
+            dependencies: [
+                .product(name: "Cordova", package: "cordova-ios"),
+                .product(name: "OneSignal", package: "OneSignal-iOS-SDK")
+            ],
+            path: "src/ios",
+            publicHeadersPath: ".")
+    ]
+)
